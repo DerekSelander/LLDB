b2d3b94e67b440d39390460b91aecc6c8458d018
Derek Selander <derekselander@yahoo.com>
2017-02-25 10:04:40 -0700

    Removed debugging output

--- lldb_commands/dslldb.py
+++ lldb_commands/dslldb.py
@@ -35,7 +35,7 @@ def load_python_scripts_dir(dir_name):
         if file.endswith('.py'):
             cmd = 'command script import ' 
         elif file.endswith('.txt'):
-            cmd = 'command source -e0 -s0 '
+            cmd = 'command source -e0 -s1 '
         else: 
             continue
 
