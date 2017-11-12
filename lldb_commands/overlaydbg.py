c87862ffc02cfb3e7b1dda9ead9eb159541d5515
Derek Selander <derekselander@yahoo.com>
2017-11-11 18:45:08 -0700

    overlaydbg cmd added
    UIDebuggingInformationOverlay iOS 11 Compatible
    See: http://ryanipete.com/blog/ios/swift/objective-c/uidebugginginformationoverlay/

---
+++ lldb_commands/overlaydbg.py
@@ -0,0 +1,76 @@
+
+
+import lldb
+import os
+import shlex
+import optparse
+
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand(
+    'command script add -f overlaydbg.handle_command overlaydbg')
+
+class GlobalProcess:
+    hasPerformedSetup = False
+
+def handle_command(debugger, command, result, internal_dict):
+    '''
+    Toggles the UIDebuggingInformationOverlay, iOS 9.X - 11.X only
+    '''
+
+    target = debugger.GetSelectedTarget()
+    if GlobalProcess.hasPerformedSetup is False:
+        setupIfiOS11(target)
+        debugger.HandleCommand('exp -lobjc -O -- [UIDebuggingInformationOverlay prepareDebuggingOverlay]')
+        script = 'exp -lobjc -O -- id _dstap = [UIGestureRecognizer new]; [_dstap setState:3]; [[UIDebuggingInformationOverlayInvokeGestureHandler mainHandler] _handleActivationGesture:_dstap];  @"Displaying UIDebuggingInformationOverlay... resume execution"'
+        debugger.HandleCommand(script)
+        GlobalProcess.hasPerformedSetup = True
+    else: 
+        debugger.HandleCommand('exp -lobjc -O -- [[UIDebuggingInformationOverlay overlay] toggleVisibility]; [UIDebuggingInformationOverlay overlay] ?  @"Displaying UIDebuggingInformationOverlay... resume execution" : @"Failure, womp"')
+
+
+
+
+def setupIfiOS11(target):
+    options = lldb.SBExpressionOptions()
+    options.SetLanguage (lldb.eLanguageTypeObjC_plus_plus)
+    options.SetCoerceResultToId()
+
+    versionval = target.EvaluateExpression('[[UIDevice currentDevice] systemVersion]', options)
+    versionvalstr = versionval.description 
+    
+    debugger = target.GetDebugger()
+
+    if "11." in versionvalstr:
+        tweakiOS11Memory(target)
+
+
+def tweakiOS11Memory(target):
+    module = target.module['UIKit']
+
+    symbolsFF = [ '__fetchedInternalDeviceOnceToken', 
+                  'UIDebuggingOverlayIsEnabled.__overlayIsEnabled',
+                  'UIDebuggingOverlayIsEnabled.onceToken',
+                  '__isInternalDevice', 
+                  '__UIGetDebuggingOverlayEnabledAssumingInternal.onceToken', 
+                  '__hasCachedDebuggingOverlayEnabled', 
+                  '__cachedDebuggingOverlayEnabled']
+
+    for symStr in symbolsFF:
+        syms = module.FindSymbols(symStr, lldb.eSymbolTypeData)
+        for symbol in syms.symbols:
+            loadAddr = symbol.GetStartAddress().GetLoadAddress(target)
+            size = symbol.GetEndAddress().GetFileAddress() - symbol.GetStartAddress().GetFileAddress()
+            debugger = target.GetDebugger()
+            cmd = 'mem write {} 0x{} -s {}'.format(loadAddr, 'ff' * size, size)
+            debugger.HandleCommand(cmd)
+
+
+    syms = module.FindSymbols('__hasCachedDebuggingOverlayEnabled', lldb.eSymbolTypeData)
+    for symbol in syms.symbols:
+        loadAddr = symbol.GetStartAddress().GetLoadAddress(target)
+        size = symbol.GetEndAddress().GetFileAddress() - symbol.GetStartAddress().GetFileAddress()
+        debugger = target.GetDebugger()
+        cmd = 'mem write {} 0xffffffffffffff01 -s 8'.format(loadAddr)
+        debugger.HandleCommand(cmd)
+
+
